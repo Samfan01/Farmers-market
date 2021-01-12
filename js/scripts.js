4de55169379f15d0b35ebc1ba84b0c2b88684535
DawnDesign7 <dawnmoraa@gmail.com>
2021-01-12 15:39:53 +0300

    Shopping cart basic html structure and js functionality added but commented out

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,26 @@ function removeClass(element, name) {
   }
   element.className = arr1.join(" ");
 }
+
+// SHOPPING CART FUNCTIONALITY
+
+// <body onload="doShowAll()">
+//
+// function doShowAll() {
+//     if (CheckBrowser()) {
+//         var key = "";
+//         var list = "<tr><th>Item</th><th>Value</th></tr>\n";
+//         var i = 0;
+//         for (i = 0; i <= localStorage.length-1; i++) {
+//             key = localStorage.key(i);
+//             list += "<tr><td>" + key + "</td>\n<td>"
+//                     + localStorage.getItem(key) + "</td></tr>\n";
+//         }
+//         if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
+//             list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+//         }
+//         document.getElementById('list').innerHTML = list;
+//     } else {
+//         alert('Cannot save shopping list as your browser does not support HTML 5');
+//     }
+// }
