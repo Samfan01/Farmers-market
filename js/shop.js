4d50706d271eee2ade639b66bb79af9e7d75bf1a
DawnDesign7 <75112477+DawnDesign7@users.noreply.github.com>
2021-01-15 10:24:35 +0300

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,1 +1,60 @@
+// SHOPPING CART FUNCTIONALITY
+// Append
+$(document).ready(function(){
+  var itemPrice = $("#price").val();
+  $(".store-item-icon").click(function(){
+    $("ul#list").append("<li><span> "+itemPrice+"</span></li>");
+    alert("Item added to cart");
+  })
+});
+
+
+
+// SHOP.HTML FUNCTION TO FILTER PRODUCTS
+filterProducts("all");
+
+function filterProducts(c) {
+  var x,i;
+  x = document.getElementsByClassName("box");
+  if (c == "all") c = "";
+  //add .show to filtered products
+  for (i = 0; i < x.length; i++){
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+  }
+}
+//Show filtered elements
+function addClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++){
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " "+ arr2[i];
+    }
+  }
+}
+//hide elements not selected
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("buttons");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
 
